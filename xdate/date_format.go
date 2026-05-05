35aa7e38b4926a7fa077df28f3b045abe12d1334
Somphong Phuangmalai <somphong.ph@gmail.com>
2026-05-05 10:09:28 +0700

    add format time

--- xdate/date_format.go
+++ xdate/date_format.go
@@ -1,6 +1,9 @@
 package xdate
 
 const (
-	FormatFrontend = "2006-01-02T15:04:05Z" // ISO 8601 format
-	FormatDateOnly = "2006-01-02"
+	FormatFrontend       = "2006-01-02T15:04:05Z" // ISO 8601 format
+	FormatDateOnly       = "2006-01-02"
+	FormatTimeOnly       = "15:04:05"
+	FormatTimeHourMinute = "15:04"
+	FormatTimeHour       = "15"
 )
