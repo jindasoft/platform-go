46bc172233861243308cd629395a573cb2770184
Somphong Phuangmalai <30485966+somphongph@users.noreply.github.com>
2026-05-05 10:39:24 +0700

    change format date (#18)

--- xdate/date_format.go
+++ xdate/date_format.go
@@ -1,9 +1,8 @@
 package xdate
 
 const (
-	FormatFrontend       = "2006-01-02T15:04:05Z" // ISO 8601 format
-	FormatDateOnly       = "2006-01-02"
-	FormatTimeOnly       = "15:04:05"
-	FormatTimeHourMinute = "15:04"
-	FormatTimeHour       = "15"
+	FormatFrontend = "2006-01-02T15:04:05Z" // ISO 8601 format
+	FormatDate     = "2006-01-02"
+	FormatTime     = "15:04:05"
+	FormatTimeHM   = "15:04"
 )
