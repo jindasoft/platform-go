1da7f2558c83f99d07026573a28a3cc79e3ade87
Somphong Phuangmalai <30485966+somphongph@users.noreply.github.com>
2026-05-11 12:37:43 +0700

    update (#21)

--- xdate/date_format.go
+++ xdate/date_format.go
@@ -1,8 +1,11 @@
 package xdate
 
 const (
-	FormatFrontend = "2006-01-02T15:04:05Z" // ISO 8601 format
-	FormatDate     = "2006-01-02"
-	FormatTime     = "15:04:05"
-	FormatTimeHM   = "15:04"
+	FormatFrontend      = "2006-01-02T15:04:05Z" // ISO 8601 format
+	FormatDate          = "2006-01-02"
+	FormatDateCompact   = "20060102"
+	FormatTime          = "15:04:05"
+	FormatTimeCompact   = "150405"
+	FormatTimeHM        = "15:04"
+	FormatTimeHMCompact = "1504"
 )
