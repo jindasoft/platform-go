943bdd56cb3f94382af4651ca9215c4faab98ac9
Somphong Phuangmalai <somphong.ph@gmail.com>
2026-05-20 14:28:58 +0700

    status

--- xentities/constant.go
+++ xentities/constant.go
@@ -7,3 +7,23 @@ const (
 	StatusInactive Status = "inactive"
 	StatusDeleted  Status = "deleted"
 )
+
+var StatusMap = map[string]Status{
+	string(StatusActive):   StatusActive,
+	string(StatusInactive): StatusInactive,
+	string(StatusDeleted):  StatusDeleted,
+}
+
+func FromStatusCode(code string) (Status, bool) {
+	status, ok := StatusMap[code]
+	return status, ok
+}
+
+func (s Status) IsValid() bool {
+	_, ok := StatusMap[string(s)]
+	return ok
+}
+
+func (s Status) String() string {
+	return string(s)
+}
